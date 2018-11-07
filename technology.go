56ff3f354f06e7edbd0d1ad0f7c4e53c638a9e05
Luc Viala <luc.viala19@gmail.com>
2018-11-07 23:14:25 +0100

    evol: now support tethering

--- technology.go
+++ technology.go
@@ -1,14 +1,21 @@
 package connman
 
-import "github.com/godbus/dbus"
+import (
+	"fmt"
+	"log"
+
+	"github.com/godbus/dbus"
+)
 
 type Technology struct {
-	Path      dbus.ObjectPath
-	Name      string
-	Type      string
-	Powered   bool
-	Connected bool
-	Tethering bool
+	Path                dbus.ObjectPath
+	Name                string
+	Type                string
+	Powered             bool
+	Connected           bool
+	Tethering           bool
+	TetheringIdentifier string
+	TetheringPassphrase string
 }
 
 func (t *Technology) Enable() error {
@@ -35,4 +42,43 @@ func (t *Technology) Scan() error {
 
 	_, err = db.Call("Scan")
 	return err
+}
+
+func (t *Technology) EnableTethering(ssid string, psk string) error {
+	db, err := DBusTechnology(t.Path)
+	if err != nil {
+		return err
+	}
+
+	properties, err := db.Call("GetProperties")
+	if err != nil {
+		log.Printf("Error: %v", err)
+		return err
+	}
+	fmt.Printf("Properties: %v\n", properties)
+	if len(ssid) > 0 {
+		log.Printf("Setting up TetheringIdentifier: %v\n", ssid)
+		db.Set("TetheringIdentifier", ssid)
+		log.Printf("Setting up TetheringIdentifier: Successfully\n")
+	}
+	if len(psk) > 0 {
+		log.Printf("Setting up TetheringPassphrase: %v\n", psk)
+		db.Set("TetheringPassphrase", psk)
+		log.Printf("Setting up TetheringPassphrase: Successfully\n")
+	} else {
+		return fmt.Errorf("Passphrase too short: %v", psk)
+	}
+	log.Printf("Enabling tethering: %v - %v\n", ssid, psk)
+	return db.Set("Tethering", true)
+
+}
+
+func (t *Technology) DisableTethering() error {
+	db, err := DBusTechnology(t.Path)
+	if err != nil {
+		log.Printf("Error: %v\n", err)
+		return err
+	}
+	log.Println("Disabling tethering!")
+	return db.Set("Tethering", false)
 }
